a681fd7a05151b47206d1b2e83cb7c491b3d9910
Nguyen Dinh Duy Thong <nguyendinhduythong@gmail.com>
2022-06-06 13:45:48 +0700

    commit

---
+++ app.py
@@ -0,0 +1,67 @@
+from asyncio import threads
+from concurrent.futures import thread
+import json
+import os
+import x
+import time
+
+from flask import Flask, render_template, redirect, request, url_for, jsonify
+from flask_restful import Api
+from werkzeug.utils import secure_filename
+from waitress import serve
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config["CSV_UPLOADS"] = 'static/files'
+app.config["ALOWED_EXSTENSION"] = ["CSV"]
+
+
+def allowed_file(filename):
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALOWED_EXSTENSION"]:
+        return True
+    else:
+        return False
+
+
+@app.route("/homepage")
+def ToHomePage():
+    return render_template("index.html")
+
+
+@app.route("/do", methods=['POST'])
+def calculated():
+    if request.method == 'POST':
+        if request.files:
+
+            file = request.files["csv"]
+
+            if file.filename == "":
+                print("csv must have a filename!")
+                return jsonify({'error': 'Missing file!'})
+
+            if not allowed_file(file.filename):
+                print("That is not allowed")
+                return jsonify({'error': 'That is not a csv file!'})
+            else:
+                ml = int(time.time() * 1000)
+                filename = secure_filename(str(ml) + file.filename)
+                file.save(os.path.join(app.config["CSV_UPLOADS"], filename))
+                print("File Saved")
+                res = x.doAll(filename)
+                res1 = res.get('LR')
+                res2 = res.get('SVR')
+                res3 = res.get('RF')
+                return render_template("result.html",result1 = res1, result2= res2, result3= res3)
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+serve(app, host='0.0.0.0', port=8080, threads=1)
---
+++ x.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import joblib
+from scipy.signal import savgol_filter
+
+
+def toPixels(pixels):
+    arr = np.array(pixels.split(), "float64")
+    arr = arr.reshape(228, )
+    return arr
+
+
+def readLRBrix(absorbance):
+    filename = 'LRBrix.pkl'
+    loaded_model = joblib.load('LRBrix.pkl')
+    ln = loaded_model.predict(absorbance.reshape(1, 228))
+    return float(ln)
+
+
+def readSVRBrix(absorbance):
+    filename = 'SVRBrix.pkl'
+    loaded_model = joblib.load('SVRBrix.pkl')
+    ln = loaded_model.predict(absorbance.reshape(1, 228))
+    return float(ln)
+
+
+def readRFBrix(absorbance):
+    filename = 'RFBrix.pkl'
+    loaded_model = joblib.load('RFBrix.pkl')
+    ln = loaded_model.predict(absorbance.reshape(1, 228))
+    return float(ln)
+
+
+def readXXX():
+    return 1
+
+
+def doAll(filename):
+    """**Principal Component Analysis**"""
+
+    data_demo = pd.read_csv("./static/files/"+filename)
+    input_demo = data_demo["Hadamard 1"][21:21 + 229]
+    input_demo = np.array(input_demo.astype('float64'))
+    input_dfeat = savgol_filter(input_demo, 25, polyorder=5, deriv=1)
+
+    Absorbance = np.array(input_dfeat)
+    data_mean = np.mean(Absorbance)
+    data_std = np.std(Absorbance)
+    one = np.ones_like(228, )
+    Absorbance = (Absorbance - one * data_mean) / data_std
+
+    d = {"LR": readLRBrix(Absorbance), "SVR": readSVRBrix(Absorbance), "RF": readRFBrix(Absorbance)}
+
+    return d
