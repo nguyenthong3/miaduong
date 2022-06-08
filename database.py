26c98fe22e1394cbfb7e9f003a9898d34444e9c8
Nguyen Dinh Duy Thong <nguyendinhduythong@gmail.com>
2022-06-08 09:24:26 +0700

    fix

---
+++ database.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from typing_extensions import Self
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Models(db.Model):
+    id=db.column(db.Integer, primary_key=True)
+    name=db.column(db.String,)
+    result = db.relationship('Result',backref="models")
+
+    def __repr__(self) -> str:
+        return 'name>>>{self.name}'
+
+class StoreFile(db.Model):
+    id=db.column(db.Integer, primary_key=True)
+    filename=db.collumn(db.String(30))
+    id_result=db.column(db.Integer)
+
+class Result(db.Model):
+    id=db.column(db.Integer, primary_key=True)
+    created_at=db.column(db.DateTime, default=datetime.now())
+    id_model=db.column(db.Integer,db.Foreignkey('Models.id'))
+    pre_result=db.column(db.Float)
+    # def __init__*(self, **kwargs):
+    #     super().__intin__()
+
